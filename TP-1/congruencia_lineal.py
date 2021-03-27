679ede6eafb68d604a51f2a08ae1d62f8b937bc4
Camila Stock <camila.stock78@gmail.com>
2021-03-26 22:32:01 -0300

    Adding while to interpret intervals

--- TP-1/congruencia_lineal.py
+++ TP-1/congruencia_lineal.py
@@ -1,35 +1,55 @@
 from decimal import Decimal, ROUND_HALF_UP
 
+
 def linearMethod(x, k, c, g):
-
-    periodo = 0
+    periodo = 1
     bandera = 0
-    a = 1+ 4 * k
-    mod = pow(2,g)
+    a = 1 + 4 * k
+    mod = pow(2, g)
 
-    while(bandera != x):
+    lista_intervalos = []
+    count_intervalo = 10
+
+    tam_intervalo = Decimal(periodo) / Decimal(count_intervalo)
+    print("tam_intervalo: {0}".format(tam_intervalo))
+
+    lista_intervalos.append(0)
+
+    print("Range: {0}".format(range(0, periodo)))
+    for i in range(0, periodo):
+        print("valor de i: {0}".format(i))
+        valor_anterior = lista_intervalos[i]
+        print("valor anterior: {0}".format(valor_anterior))
+        print("tam_intervalo: {0}".format(tam_intervalo))
+        aux = valor_anterior + tam_intervalo
+        print("Valor anterior + intrevalo: {0}".format(aux))
+        lista_intervalos.append(aux)
+
+
+    print("### Lista de intervalos: {0}".format(lista_intervalos))
+    while (bandera != x):
         if (periodo == 0):
             bandera = x
-        x =  ( (a * x + c) % mod )
-        ran =Decimal(x) / Decimal( mod - 1 )
+        x = ((a * x + c) % mod)
+        ran = Decimal(x) / Decimal(mod - 1)
 
         output = Decimal(Decimal(ran).quantize(Decimal('.0001'), rounding=ROUND_HALF_UP))
         print(output)
         periodo = periodo + 1
 
-'''
-    if(periodo == mod):
-        print("El periodo es completo: ", periodo)
-    else:
-        print("El periodo es incompleto:", periodo)
-'''
+#    if(periodo == mod):
+#        print("El periodo es completo: ", periodo)
+#    else:
+#        print("El periodo es incompleto:", periodo)
+
 
 def main():
-    x = int(raw_input("Introduce el valor de la semilla: "))
-    k = int(raw_input("Introduce el valor de k: "))
-    c = int(raw_input("Introduce el valor de c: "))
-    g = int(raw_input("Introduce el valor de g: "))
-    linearMethod(x,k,c,g)
+    x = int(input("Introduce el valor de la semilla: "))
+    k = int(input("Introduce el valor de k: "))
+    c = int(input("Introduce el valor de c: "))
+    g = int(input("Introduce el valor de g: "))
+    linearMethod(x, k, c, g)
+
 
 if __name__ == "__main__":
     main()
