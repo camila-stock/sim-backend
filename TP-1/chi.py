317f1bbd1bf8255eb8a78651575e7b2b9588a9dd
Macarena Palacios <maca.palacios.311@gmail.com>
2021-03-30 19:20:28 -0300

    Update intervals

--- TP-1/chi.py
+++ TP-1/chi.py
@@ -14,11 +14,12 @@ def chiMethod(data):
         print("c: {}".format(c))
         ca = ca + c
         print("ca: {}".format(ca))
-        lista_chi.append(Chi(data[i].frecuencia, fe, c,ca))
+        lista_chi.append(Chi(data[i].cota_superior,data[i].frecuencia, fe, c,ca))
     return lista_chi
 
 class Chi:
-  def __init__(self, fo, fe, C, CA):
+  def __init__(self, intervalo, fo, fe, C, CA):
+    self.intervalo = intervalo
     self.fo = fo
     self.fe = fe
     self.C = C
